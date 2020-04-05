27616d0e27a382bcdf4b6797bf826c9250b27bc8
Samantha Jimenez <samantha@Samanthas-MacBook-Pro.local>
2020-04-05 15:30:03 -0400

    complete pokemonDisplay js and css

--- unit4assessment/src/components/PokemonDisplay.js
+++ unit4assessment/src/components/PokemonDisplay.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../css/PokemonDisplay.css';
-import Axios from 'axios';
-
+import axios from 'axios';
+import Comments from './Comments';
 
 const PokemonDisplay = () => {
+    const [toggle, setToggle] = useState(true);
     const [types, setTypes] = useState([]);
     const [name, setName] = useState('');
-    const [photo, setPhoto] = useState('');
+    const [showPhoto, setShowPhoto] = useState({});
     const location = useLocation();
     let url = location.state.url;
 
     const getPokeInfo = async () => {
         try {
-            let pokeInfo = await Axios.get(url);
+            let pokeInfo = await axios.get(url);
             setTypes(pokeInfo.data.types);
             setName(pokeInfo.data.species.name);
-            setPhoto(pokeInfo.data.sprites.front_default)
+            setShowPhoto(pokeInfo.data.sprites);
         } catch(error) {
             setTypes([]);
             setName('');
@@ -26,39 +27,32 @@ const PokemonDisplay = () => {
 
     useEffect(() => {
         getPokeInfo();
-    }, )
-
-    const showPokeType = types.map((el) => {
-        return <li>{el.type.name}</li>
     })
 
-    const handleComment = () => {
+    const showPokeType = types.map((el) => {
+        return <li className='typeLi' key={el.type.name}>{el.type.name}</li>
+    })
 
+    const handleClick = (e) => {
+        e.preventDefault();
+        setToggle(!toggle);
     }
-    // see an individual Pokemon's types, sprites, and leave comments
 
     return(
         <div className='DisplayDiv'>
             <div className='split left'>
                 <div className='centered'>
                     <h1>Name: <strong>{name}</strong></h1>
-                    <p>Type: <strong>{showPokeType}</strong></p>
-                    <img src={photo} alt=''/>
+                    <p className='leftP'>Type: <strong>{showPokeType}</strong></p>
+                    {toggle ? <img className='leftImg' src={showPhoto.front_default} alt=''/> : <img className='leftImg' src={showPhoto.back_default} alt=''/>}
+                    <form className='spriteForm'>
+                        <button className='spriteBtn' type='click' onClick={handleClick}>Turn</button>
+                    </form>
                 </div>
             </div>
             <div className='split right'>
                 <div className='centered'>
-                    <form onSubmit={handleComment}>
-                        <label>
-                            Name:
-                            <input id='name' type='text' placeholder='Name..'></input>
-                        </label>
-                        <label>
-                            Comment:
-                            <input id='comment' type='text' placeholder='Comment..'></input>
-                        </label>
-                        <button>Submit</button>
-                    </form>
+                    <Comments />
                 </div>
             </div>
         </div>
