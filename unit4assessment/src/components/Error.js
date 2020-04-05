dd3b7619cb6a0ae5f1b3ac5ba1181627e74afbb0
Samantha Jimenez <samantha@Samanthas-MacBook-Pro.local>
2020-04-05 15:24:45 -0400

    complete error page js and css

--- unit4assessment/src/components/Error.js
+++ unit4assessment/src/components/Error.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../css/Error.css';
 
 const Error = () => {
-    
     return(
         <div className='ErrorDiv'>
-
+            <h1>Sorry!</h1>
+            <h2>Page Not Found</h2>
         </div>
     );
 };
